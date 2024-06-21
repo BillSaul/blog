f1f52e024382a7df63696bcf111e1dad3641b25f
BillSaul <wiztor@163.com>
2024-06-21 17:30:30 +0800

    add tracking script

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,6 +11,48 @@ export default defineUserConfig({
   title: "秋澪冬安",
   description: "个人技术博客，记录和分享各种技术知识",
 
+  head: [
+    // 百度统计脚本
+    [
+      "script",
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        let isLocal = window.location.hostname.includes("localhost");
+        if (!isLocal) {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?b3eec175381ad9162a641e5fb8bd0548";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        }
+      })();
+      `,
+    ],
+    // 腾讯云前端性能监控
+    ["script", { src: "https://tam.cdn-go.cn/aegis-sdk/latest/aegis.min.js" }],
+    [
+      "script",
+      {},
+      `
+      (function() {
+        let isLocal = window.location.hostname.includes("localhost");
+        if (!isLocal) {
+          if (typeof Aegis === 'function') {
+            var aegis = new Aegis({
+              id: 'lJxq1HyLrObPoynRlJ', // 上报 id
+              reportApiSpeed: true, // 接口测速
+              reportAssetSpeed: true, // 静态资源测速
+              spa: true, // spa 应用页面跳转的时候开启 pv 计算
+            hostUrl: 'https://rumt-zh.com'
+            });
+          }
+        }
+      })();
+      `,
+    ],
+  ],
+
   theme,
 
   alias: {
