200eb4cae6aec436a8edbfcb7cf3e7c0bef35e8e
BillSaul <wiztor@163.com>
2024-06-24 16:41:34 +0800

    on the right side of the homepage, "category" is selected by default

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -4,5 +4,33 @@ import runtime from "./components/runtime.vue";
 export default defineClientConfig({
   enhance: ({ app, router, siteData }) => {
     app.component("runtime", runtime);
+
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    router.afterEach((to, from) => {
+      if (
+        typeof window !== "undefined" &&
+        (to.path === "/" || to.path !== from.path)
+      ) {
+        if (timeoutId) clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+          const elements = document.querySelectorAll(".vp-blog-type-switcher");
+          elements.forEach((el) => {
+            const buttons = el.querySelectorAll("button");
+            if (/^\/article\//.test(to.path) && buttons.length > 0) {
+              buttons[0].click();
+            } else if (/^\/category\//.test(to.path) && buttons.length > 1) {
+              buttons[1].click();
+            } else if (/^\/tag\//.test(to.path) && buttons.length > 2) {
+              buttons[2].click();
+            } else if (/^\/timeline\//.test(to.path) && buttons.length > 3) {
+              buttons[3].click();
+            } else if (buttons.length > 1) {
+              buttons[1].click();
+            }
+          });
+        }, 500);
+      }
+    });
   },
 });
